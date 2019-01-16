db81f1f712a24ede396f3c437f97ce5b807ee995
Damien Lespiau <damien@weave.works>
2019-01-16 16:59:10 +0000

    quick-start: Add the alice.js example

---
+++ quick-start/alice.js
@@ -0,0 +1,18 @@
+// Import jk standard library for the file writing function.
+import std from 'std';
+
+// Define a developer.
+const alice = {
+    name: 'Alice',
+    beverage: 'Club-Mate',
+    monitors: 2,
+    languages: [
+        'python',
+        'haskell',
+        'c++',
+        '68k assembly', // Alice is cool like that!
+    ],
+};
+
+// Write the developer description as YAML.
+std.write(alice, `developers/${alice.name.toLowerCase()}.yaml`);
