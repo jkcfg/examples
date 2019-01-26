25d30a18a7916de5eb5b4df9bf846a647029ad6e
Damien Lespiau <damien@weave.works>
2019-01-26 16:06:13 +0000

    quick-start: Add a terraform example

---
+++ quick-start/github.js
@@ -0,0 +1,27 @@
+import std from 'std';
+import { developers } from 'developers.js';
+
+// Github organization
+const organization = 'myorg';
+
+// Describe role and membership as defined by the Github terraform provider.
+const role = developer => developer.github.admin ? 'admin' : 'member';
+const membershipId = developer => `${organization}_${developer.github.handle}`;
+
+const config = {
+    provider: {
+        github: {
+            organization,
+        },
+    },
+    github_membership: {},
+};
+
+for (const dev of developers) {
+    config.github_membership[membershipId(dev)] = {
+        username: dev.github.handle,
+        role: role(dev),
+    };
+}
+
+std.write(config, 'terraform/github.json');
